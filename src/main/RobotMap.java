b2c6e7ae820f3bed3bdab87020fb6aa5f9ab7c25
ashwink0 <ashwin.kuppahally@gmail.com>
2020-12-03 17:02:31 -0800

    Add Swerve Module Class

---
+++ src/main/RobotMap.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public static final int[] DRIVE_IDS = { 4, 4, 1, 1 }; // TL, TR, BL, BR
+    public static final int LOOP_INDEX = 0;
+    // public static final int[] ANGLE_IDS = {1, 3, 5, 7};
+
+    public static final int SLOT_INDEX = 0;
+    public static final int DRIVER_PORT = 0;
+    public static final int OPERATOR_PORT = 1;
+
+    public static final int FLOWER_FORWARD_PORT = 0;
+    public static final int FLOWER_REVERSE_PORT = 0;
+    public static final int EXTENDER_FORWARD_PORT = 0;
+    public static final int EXTENDER_REVERSE_PORT = 0;
+
+    public static final int INTAKE_FORWARD_PORT = 0;
+    public static final int INTAKE_REVERSE_PORT = 0;
+
+    public static final int INTAKE = 0;
+
+}

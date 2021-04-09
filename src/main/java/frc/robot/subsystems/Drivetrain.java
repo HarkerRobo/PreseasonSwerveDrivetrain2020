17b86c26c58365dc3dc10befc363bec4604e010d
CMEONE <contact@kabirwearsato.ga>
2021-04-08 17:20:30 -0700

    Start work on kinematics

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.util.SwerveModule;
@@ -30,6 +32,19 @@ public class Drivetrain extends SubsystemBase {
     private boolean BOTTOM_RIGHT_TRANSLATION_SENSOR_PHASE = false;
     private boolean BOTTOM_RIGHT_INVERTED = false;
 
+    private Translation2d m_frontLeftLocation;
+    private Translation2d m_frontRightLocation;
+    private Translation2d m_backLeftLocation;
+    private Translation2d m_backRightLocation;
+    private SwerveDriveKinematics m_kinematics;
+
+    public static final double DT_WIDTH = 0.419;
+    public static final double DT_LENGTH = 0.523;
+    public static final double TALON_PEAK_LIMIT = 20;
+    public static final double TALON_PEAK_TIME = 0.750;
+    public static final double TALON_CONTINUOUS_LIMIT = 15;
+    public static final double VOLTAGE_COMP = 10;
+
     public Drivetrain() {
         topLeft = new SwerveModule(TOP_LEFT_ROTATION_SENSOR_PHASE, TOP_LEFT_TRANSLATION_SENSOR_PHASE,
                 RobotMap.ROTATION_IDS[0], RobotMap.TRANSLATION_IDS[0], TOP_LEFT_INVERTED);
@@ -39,6 +54,26 @@ public class Drivetrain extends SubsystemBase {
                 RobotMap.ROTATION_IDS[2], RobotMap.TRANSLATION_IDS[2], BOTTOM_LEFT_INVERTED);
         bottomRight = new SwerveModule(BOTTOM_RIGHT_ROTATION_SENSOR_PHASE, BOTTOM_RIGHT_TRANSLATION_SENSOR_PHASE,
                 RobotMap.ROTATION_IDS[3], RobotMap.TRANSLATION_IDS[3], BOTTOM_RIGHT_INVERTED);
+
+        m_frontLeftLocation = new Translation2d(-DT_LENGTH / 2, DT_WIDTH / 2);
+        m_frontRightLocation = new Translation2d(DT_LENGTH / 2, DT_WIDTH / 2);
+        m_backLeftLocation = new Translation2d(-DT_LENGTH / 2, -DT_WIDTH / 2);
+        m_backRightLocation = new Translation2d(DT_LENGTH / 2, -DT_WIDTH / 2);
+        m_kinematics = new SwerveDriveKinematics(m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+        // In SwerveManual:
+
+        // TODO: Add multiplier to raw outputs for safety
+
+        // TODO: Make ChassisSpeeds object (vx, vy, omega)
+
+        // TODO: Convert from ChassisSpeeds to SwerveModuleStates
+
+        // In DriveTrain:
+        
+        // Set velocities based on SwerveModuleStates' speeds and angles,
+        // make sure to convert from angle degrees to encoder units before
+        // implementing angle PID
     }
 
     public SwerveModule getTopLeft() {
--- src/main/java/frc/robot/util/Vector.java
+++ src/main/java/frc/robot/util/Vector.java
@@ -9,11 +9,31 @@ public class Vector {
         this.y = y;
     }
 
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
     public double getMagnitude() {
         return Math.sqrt((x * x) + (y * y));
     }
 
     public double getAngle() {
         return Math.toDegrees(Math.atan2(y, x));
+    }
+
+    public static Vector add(Vector vector1, Vector vector2) {
+        double newVectorX = vector1.getX() + vector2.getX();
+        double newVectorY = vector1.getY() + vector2.getY();
+        return new Vector(newVectorX, newVectorY);
+    }
+
+    public static Vector multiply(Vector vector, double multiplier) {
+        double newVectorX = vector.getX() * multiplier;
+        double newVectorY = vector.getY() * multiplier;
+        return new Vector(newVectorX, newVectorY);
     }
 }

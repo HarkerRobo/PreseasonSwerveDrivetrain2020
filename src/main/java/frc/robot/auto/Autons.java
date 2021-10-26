a6d693f20efea53543663bf6e496a211674b41b6
Ashwin K <ashwin.kuppahally@gmail.com>
2021-10-25 19:05:45 -0700

    Add shooter and intake to auto

--- src/main/java/frc/robot/auto/Autons.java
+++ src/main/java/frc/robot/auto/Autons.java
@@ -5,6 +5,10 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.drivetrain.HSSwerveDriveController;
 import frc.robot.commands.intake.IntakeAutonControlForward;
+import frc.robot.commands.intake.MoveBallsToShooter;
+import frc.robot.commands.shooter.ShootWithHighHood;
+import frc.robot.commands.shooter.ShootWithLimelight;
+import frc.robot.commands.shooter.ShooterRevUp;
 
 public class Autons {
     // public static SequentialCommandGroup autoPath1 = new SequentialCommandGroup(
@@ -14,26 +18,65 @@ public class Autons {
 
     public static SequentialCommandGroup stealBallsFromTrench = new SequentialCommandGroup(
         new HSSwerveDriveController(Trajectories.chezy_moveToBalls, Rotation2d.fromDegrees(0)),
-        new HSSwerveDriveController(Trajectories.chezy_moveToShootingLocation, Rotation2d.fromDegrees(0)
-    ));
+        new ParallelCommandGroup(
+            new HSSwerveDriveController(Trajectories.chezy_moveToShootingLocation, Rotation2d.fromDegrees(0)),
+            new ShooterRevUp()
+        ),
+        new ParallelCommandGroup(
+            new ShootWithHighHood(),
+            new MoveBallsToShooter()
+        )
+    );
 
     public static SequentialCommandGroup leftToUpAgainstGoal = new SequentialCommandGroup(
-        new HSSwerveDriveController(Trajectories.chezy_leftInitiationToScoringZone, Rotation2d.fromDegrees(0))
+        new ParallelCommandGroup(
+            new HSSwerveDriveController(Trajectories.chezy_leftInitiationToScoringZone, Rotation2d.fromDegrees(0)),
+            new ShooterRevUp()
+        ),
+        new ParallelCommandGroup(
+            new ShootWithHighHood(),
+            new MoveBallsToShooter()
+        )
     );
 
     public static SequentialCommandGroup centerToUpAgainstGoal = new SequentialCommandGroup(
-        new HSSwerveDriveController(Trajectories.chezy_centerInitiationToScoringZone, Rotation2d.fromDegrees(0))
+        new ParallelCommandGroup(
+            new HSSwerveDriveController(Trajectories.chezy_centerInitiationToScoringZone, Rotation2d.fromDegrees(0)),
+            new ShooterRevUp()
+        ),
+        new ParallelCommandGroup(
+            new ShootWithHighHood(),
+            new MoveBallsToShooter()
+        )
     );
 
     public static SequentialCommandGroup rightToUpAgainstGoal = new SequentialCommandGroup(
-        new HSSwerveDriveController(Trajectories.chezy_rightInitiationToScoringZone, Rotation2d.fromDegrees(0))
+        new ParallelCommandGroup(
+            new HSSwerveDriveController(Trajectories.chezy_rightInitiationToScoringZone, Rotation2d.fromDegrees(0)),
+            new ShooterRevUp()
+        ),
+        new ParallelCommandGroup(
+            new ShootWithHighHood(),
+            new MoveBallsToShooter()
+        )
     );
 
     public static SequentialCommandGroup throughTrench = new SequentialCommandGroup(
         new HSSwerveDriveController(Trajectories.chezy_setUpLeftTrench, Rotation2d.fromDegrees(0)),
-        new HSSwerveDriveController(Trajectories.chezy_moveThroughTrench, Rotation2d.fromDegrees(0)),
+        new ParallelCommandGroup(
+            new HSSwerveDriveController(Trajectories.chezy_moveThroughTrench, Rotation2d.fromDegrees(0)),
+            new IntakeAutonControlForward(0.5)
+        ),
+        
         new HSSwerveDriveController(Trajectories.chezy_moveBackThroughTrench, Rotation2d.fromDegrees(0)),
-        new HSSwerveDriveController(Trajectories.chezy_shootTrenchBalls, Rotation2d.fromDegrees(0))
+        new ParallelCommandGroup(
+            new HSSwerveDriveController(Trajectories.chezy_shootTrenchBalls, Rotation2d.fromDegrees(0)),
+            new ShooterRevUp()
+        ),
+        new ParallelCommandGroup(
+            new ShootWithHighHood(),
+            new MoveBallsToShooter()
+        )
 
     );
 

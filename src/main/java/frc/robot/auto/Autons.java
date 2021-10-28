87f20d4ff975baea8555b1d436741fe8b3b69a2c
Ashwin K <ashwin.kuppahally@gmail.com>
2021-10-28 11:59:55 -0700

    Change auto to race and deadline groups

--- src/main/java/frc/robot/auto/Autons.java
+++ src/main/java/frc/robot/auto/Autons.java
@@ -1,14 +1,19 @@
 package frc.robot.auto;
 
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.drivetrain.HSSwerveDriveController;
 import frc.robot.commands.intake.IntakeAutonControlForward;
 import frc.robot.commands.intake.MoveBallsToShooter;
 import frc.robot.commands.shooter.ShootWithHighHood;
 import frc.robot.commands.shooter.ShootWithLimelight;
 import frc.robot.commands.shooter.ShooterVelocityManual;
+import frc.robot.subsystems.Intake;
 
 public class Autons {
     // public static SequentialCommandGroup autoPath1 = new SequentialCommandGroup(
@@ -18,44 +23,48 @@ public class Autons {
 
     public static SequentialCommandGroup stealBallsFromTrench = new SequentialCommandGroup(
         new HSSwerveDriveController(Trajectories.chezy_moveToBalls, Rotation2d.fromDegrees(0)),
-        new ParallelCommandGroup(
+        new ParallelRaceGroup(
             new HSSwerveDriveController(Trajectories.chezy_moveToShootingLocation, Rotation2d.fromDegrees(0)),
             new ShooterVelocityManual(65)
         ),
-        new ParallelCommandGroup(
+        new ParallelDeadlineGroup(
+            new WaitCommand(5),
             new ShootWithHighHood(),
             new MoveBallsToShooter()
         )
     );
 
     public static SequentialCommandGroup leftToUpAgainstGoal = new SequentialCommandGroup(
-        new ParallelCommandGroup(
+        new ParallelRaceGroup(
             new HSSwerveDriveController(Trajectories.chezy_leftInitiationToScoringZone, Rotation2d.fromDegrees(0)),
             new ShooterVelocityManual(65)
         ),
-        new ParallelCommandGroup(
+        new ParallelDeadlineGroup(
+            new WaitCommand(5),
             new ShootWithHighHood(),
             new MoveBallsToShooter()
         )
     );
 
     public static SequentialCommandGroup centerToUpAgainstGoal = new SequentialCommandGroup(
-        new ParallelCommandGroup(
+        new ParallelRaceGroup(
             new HSSwerveDriveController(Trajectories.chezy_centerInitiationToScoringZone, Rotation2d.fromDegrees(0)),
             new ShooterVelocityManual(65)
         ),
-        new ParallelCommandGroup(
+        new ParallelDeadlineGroup(
+            new WaitCommand(5),
             new ShootWithHighHood(),
             new MoveBallsToShooter()
         )
     );
 
     public static SequentialCommandGroup rightToUpAgainstGoal = new SequentialCommandGroup(
-        new ParallelCommandGroup(
+        new ParallelRaceGroup(
             new HSSwerveDriveController(Trajectories.chezy_rightInitiationToScoringZone, Rotation2d.fromDegrees(0)),
             new ShooterVelocityManual(65)
         ),
-        new ParallelCommandGroup(
+        new ParallelDeadlineGroup(
+            new WaitCommand(5),
             new ShootWithHighHood(),
             new MoveBallsToShooter()
         )
@@ -63,17 +72,21 @@ public class Autons {
 
     public static SequentialCommandGroup throughTrench = new SequentialCommandGroup(
         new HSSwerveDriveController(Trajectories.chezy_setUpLeftTrench, Rotation2d.fromDegrees(0)),
-        new ParallelCommandGroup(
+        new InstantCommand(() -> {
+            Intake.getInstance().invertSolenoid();
+        }),
+        new ParallelRaceGroup(
             new HSSwerveDriveController(Trajectories.chezy_moveThroughTrench, Rotation2d.fromDegrees(0)),
             new IntakeAutonControlForward(0.5)
         ),
         
         new HSSwerveDriveController(Trajectories.chezy_moveBackThroughTrench, Rotation2d.fromDegrees(0)),
-        new ParallelCommandGroup(
+        new ParallelRaceGroup(
             new HSSwerveDriveController(Trajectories.chezy_shootTrenchBalls, Rotation2d.fromDegrees(0)),
             new ShooterVelocityManual(65)
         ),
-        new ParallelCommandGroup(
+        new ParallelDeadlineGroup(
+            new WaitCommand(5),
             new ShootWithHighHood(),
             new MoveBallsToShooter()
         )
